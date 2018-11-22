9ea7326cbb76f1f3a7d4b7c453284c7c9f362265
OlafurE <olieinar@hotmail.com>
2018-11-22 22:19:11 +0000

    javascript

---
+++ src/index.js
@@ -0,0 +1,13 @@
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  const page = document.querySelector('body');
+  const isLecturePage = page.classList.contains('lecture-page');
+
+  if (isLecturePage) {
+
+  } else {
+    const list = new List();
+    list.load();
+  }
+});
